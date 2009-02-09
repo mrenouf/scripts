1fecfe10223352f769b1510ea9d8a660cfa18da6
Mark Renouf <mark.renouf@gmail.com>
2009-02-09 13:31:38 -0500

    Avoid generating method names containing Java reserved keywords

--- bundle_images.py
+++ bundle_images.py
@@ -3,6 +3,7 @@
 import sys;
 import os;
 import re;
+from sets import Set
 
 def main(argv=None):
   if argv is None:
@@ -12,7 +13,18 @@ def main(argv=None):
   p = re.compile(r"(.*)\.(png|jpg|jpeg|gif)$", re.I)
   r = re.compile(r"[-_\(\)#\$\*\&%@!^]");
   cc = re.compile(r"(\s+)([a-z]\S*)");
-  
+
+  # Java reserved keywords:
+  # http://java.sun.com/docs/books/tutorial/java/nutsandbolts/_keywords.html
+  reserved = Set(['abstract', 'assert', 'boolean', 'break', 
+    'byte', 'case', 'catch', 'char', 'class', 'const', 'continue', 
+    'default', 'do', 'double', 'else', 'enum', 'extends', 'final',
+    'finally', 'float', 'for', 'goto', 'if', 'implements', 'import', 
+    'instanceof', 'int', 'interface', 'long', 'native', 'new', 
+    'package', 'private', 'protected', 'public', 'return', 'short', 
+    'static', 'strictfp', 'super', 'switch', 'synchronized', 'this', 
+    'throw', 'throws', 'transient', 'try', 'void', 'volatile', 'while'])
+        
   print "public interface MyImageBundle extends ImageBundle {"
   dirs = os.listdir(path);
   for entry in dirs:
@@ -23,6 +35,8 @@ def main(argv=None):
         name = m.group(1)
         name = r.sub(" ", name)
         name = cc.sub(lambda m : m.group(2).title(), name)
+        if name in reserved:
+            name += "_";
         print "  @Resource(\"%s\")" % entry
         print "  public AbstractImagePrototype %s();" % name
         print
@@ -32,3 +46,4 @@ def main(argv=None):
 if __name__ == "__main__":
   sys.exit(main())
 
+ 
